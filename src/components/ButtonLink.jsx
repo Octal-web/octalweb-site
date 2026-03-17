86df9aaa993ae997f8e946a43ae27c9488e01131
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-17 15:55:53 -0300

    feat: adiciona componente ButtonLink

---
+++ src/components/ButtonLink.jsx
@@ -0,0 +1,13 @@
+export function ButtonLink({ href, children, variant = "primary" }) {
+
+    const styles = {
+        primary: "px-6 py-2 border border-brand text-brand rounded hover:bg-brand hover:text-black transition",
+        secondary:"px-6 py-2 border border-white rounded hover:bg-white hover:text-black transition"
+    };
+
+  return (
+        <a href={href} target="_blank" className={styles[variant]}>
+            {children}
+        </a>
+  )
+};

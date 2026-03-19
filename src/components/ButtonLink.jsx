1db55b4b9a8733eb878be0a2b4f6a5ffd7edbba3
Francine02 <francine.c.oliveiracruz@gmail.com>
2026-03-19 10:03:42 -0300

    refactor: estilo do botão

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -1,13 +1,14 @@
-export function ButtonLink({ href, children, variant = "primary" }) {
-
-    const styles = {
-        primary: "px-6 py-2 border border-brand text-brand rounded hover:bg-brand hover:text-black transition",
-        secondary:"px-6 py-2 border border-white rounded hover:bg-white hover:text-black transition"
-    };
+export function ButtonLink({ href, children, variant = 'primary' }) {
+  const styles = {
+    primary:
+      'px-3.5 sm:px-6 py-2 border border-brand text-brand rounded hover:bg-brand hover:text-black transition',
+    secondary:
+      'px-3.5 sm:px-6 py-2 border border-white rounded hover:bg-white hover:text-black transition',
+  };
 
   return (
-        <a href={href} target="_blank" className={styles[variant]}>
-            {children}
-        </a>
-  )
-};
+    <a href={href} target="_blank" className={styles[variant]}>
+      {children}
+    </a>
+  );
+}
